77ad8f126c833f4977e82eac3ecbf536401dc487
Eduard <krylov2310@gmail.com>
2025-11-26 20:26:55 +0500

    Update circle and square print result

--- circle.py
+++ circle.py
@@ -8,3 +8,7 @@ def area(r):
 def perimeter(r):
     return 2 * math.pi * r
 
+
+r = 5
+print(f'Area: {r} = {round(area(r), 2)}')
+print(f'Perimeter: {r} = {round(perimeter(r), 2)}')
--- square.py
+++ square.py
@@ -5,3 +5,8 @@ def area(a):
 
 def perimeter(a):
     return 4 * a
+
+
+a = 3
+print(f'Area: {a} = {round(area(a), 2)}')
+print(f'Perimeter: {a} = {round(perimeter(a), 2)}')
